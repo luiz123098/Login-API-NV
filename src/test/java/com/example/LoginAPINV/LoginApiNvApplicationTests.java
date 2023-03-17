81f1599e6e175ebeed2a5288f150b258b909962e
Victor Alves <victor.souza@sigep.com.br>
2023-03-17 08:14:30 -0300

    receba garaio - version pom.xml, ajuste no maven

--- src/test/java/com/example/LoginAPINV/LoginApiNvApplicationTests.java
+++ src/test/java/com/example/LoginAPINV/LoginApiNvApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.LoginAPINV;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration
 class LoginApiNvApplicationTests {
 
 	@Test
