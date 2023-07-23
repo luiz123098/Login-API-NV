d2c293e6c2c2d61c62fefed848fd1d353a2f7241
Neto <luiznetopaluti@hotmail.com>
2023-07-23 16:00:43 -0300

    adicionando pequenas coisas

--- src/main/java/com/exemple/Repository/UserRepository.java
+++ src/main/java/com/exemple/Repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.exemple.Repository;
 import com.exemple.model.dto.UserDTO;
 import com.exemple.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
